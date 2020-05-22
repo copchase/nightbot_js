667d33783436764bc0f7b70e8f4b73b081755300
copchase <???>
2020-05-22 09:06:37 -0700

    Add original version of 8ball_tsm_theoddone

---
+++ 8ball_tsm_theoddone.js
@@ -0,0 +1,16 @@
+const responses = [
+    "What kind of question is that? FeelsWeirdMan 👉 🚪",
+    "Stop asking me stupid questions AYAYAWeird ",
+    "All signs point to yes...",
+    "My sources say nope.",
+    "It is decidedly so!",
+    "Very doubtful.",
+    "Yes, definitely!",
+    "It is certain!",
+    "Most likely.",
+    "Outlook good.",
+    "Don\"t count on it.",
+    "I\'m going to leave before you ask any more idiotic questions peepoLeave"
+];
+
+responses[Math.floor(Math.random * responses.length)];
